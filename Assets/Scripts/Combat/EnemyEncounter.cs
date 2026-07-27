9ae89b8ab2355bbcc9d9d6ddebad6b434f3a74a5
david-sanchez17 <david.sanchez17@g.austincc.edu>
2026-07-26 22:22:05 -0500

    Greybox of dungeon

--- Assets/Scripts/Combat/EnemyEncounter.cs
+++ Assets/Scripts/Combat/EnemyEncounter.cs
@@ -1,9 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 public class EnemyEncounter : MonoBehaviour
 {
+    [SerializeField] private Transform target;
+    private NavMeshAgent agent;
     private string combatSceneName = "CombatScene";
     private bool encounterStarted = false;
+
+    void Awake()
+    {
+        if(target == null)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            if(player != null)
+            {
+                target = player.transform;
+            }
+        }
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.position);
+    }
+
+
 
     private void OnTriggerEnter(Collider other)
     {
