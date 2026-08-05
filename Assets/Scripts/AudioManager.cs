65ae3eb3adf3c27469273bca249e1d5c48ba315d
david-sanchez17 <david.sanchez17@g.austincc.edu>
2026-08-05 16:30:04 -0500

    Adjusted audio manager to support sliders and saving audio/volume changes

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,9 +2,51 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager Instance;
     [SerializeField] private AudioSource musicSource;
     [SerializeField] private AudioSource sfxSource;
-    [SerializeField] private float ambientSource;
+
+    private const string MusicVolumeKey = "MusicVolume";
+    private const string SFXVolumeKey = "SFXVolume";
+    private const float DefaultVolume = 1f;
+
+    private void Awake()
+    {
+        Instance = this;
+        LoadAudioSettings();
+    }
+
+    private void LoadAudioSettings()
+    {
+        float musicVolume = PlayerPrefs.GetFloat(MusicVolumeKey, DefaultVolume);
+        float sfxVolume = PlayerPrefs.GetFloat(SFXVolumeKey, DefaultVolume);
+        musicSource.volume = musicVolume;
+        sfxSource.volume = sfxVolume;
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+        PlayerPrefs.SetFloat(MusicVolumeKey, volume);
+        PlayerPrefs.Save();
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+        PlayerPrefs.SetFloat(SFXVolumeKey, volume);
+        PlayerPrefs.Save();
+    }
+
+    public float GetMusicVolume()
+    {
+        return musicSource.volume;
+    }
+
+    public float GetSFXVolume()
+    {
+        return sfxSource.volume;
+    }
 
     public void PlayMusic(AudioClip music)
     {
@@ -15,6 +57,33 @@ public class AudioManager : MonoBehaviour
         musicSource.clip = music;
         musicSource.loop = true;
         musicSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        if (musicSource == null)
+        {
+            return;
+        }
+        musicSource.Stop();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        if (sfxSource == null || clip == null)
+        {
+            return;
+        }
+        sfxSource.PlayOneShot(clip);
+    }
+
+    public void StopSFX()
+    {
+        if (sfxSource == null)
+        {
+            return;
+        }
+        sfxSource.Stop();
     }
 
     void Start()
